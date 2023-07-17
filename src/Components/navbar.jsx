9c2bc3fb468b8944b5caa9361c527cd63feea95c
l.ahmadi@fanap-infra.ir <l.ahmadi@fanap-infra.ir>
2023-07-17 09:13:42 +0330

    create project

---
+++ src/Components/navbar.jsx
@@ -0,0 +1,25 @@
+import { RiShoppingCartLine } from "react-icons/ri";
+import React from "react";
+import { useCartContext } from "../contex";
+
+
+const Navbar = () => {
+
+  const {cart} = useCartContext();
+ 
+  return (
+    <header className="navbar navbar-dark bg-dark shadow-sm">
+      <div className="container">
+        <a className="navbar-tool ms-3">
+          <div className="navbar-tool-icon-box bg-secondary">
+            <span className="navbar-tool-label">{cart.length}</span>
+            <RiShoppingCartLine className="navbar-tool-icon" />
+          </div>
+        </a>
+        <p className="h3 mb-0 text-light">useContext & useReducer</p>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/contex.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+
+const CartContext = createContext();
+
+const initialState = {
+    loading : false,
+    cart : cartItems,
+    total :0,
+    amount:0,
+}
+
+ const CartProvider = ({children}) => {
+    // create reducer 
+    const [state, dispatch] = useReducer(reducer , initialState);
+   
+    const clearCart = () => {
+        dispatch({type:"CLEAR_CART"});
+    }
+
+    const remove = (id) => {
+        dispatch({type:"REMOVE", payload:{id}});
+    }
+
+    const changeQuantity = changeObj => {
+        dispatch({type:"CHANGE_QUANTITY" , payload:changeObj})
+    }
+
+    useEffect(()=>{
+        dispatch({type:"GET_TOTALS"})
+    },[state.cart])
+
+
+
+
+
+    return <CartContext.Provider value={{...state,clearCart, remove, changeQuantity}}>
+        {children}
+    </CartContext.Provider>
+}
+
+// custom useContext
+ const useCartContext = () => {
+    return useContext(CartContext);
+}
+
+export {useCartContext, CartProvider}
